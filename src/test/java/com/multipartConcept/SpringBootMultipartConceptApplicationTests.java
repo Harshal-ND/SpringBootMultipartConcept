fd6a62aa36231ab655eaf7ecd1129f12dde09a5a
Harshal Dhongadi <harshal.dhongadi@anmsoft.com>
2024-05-08 18:34:49 +0530

    First Commit after creating remote Repo

---
+++ src/test/java/com/multipartConcept/SpringBootMultipartConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multipartConcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultipartConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
