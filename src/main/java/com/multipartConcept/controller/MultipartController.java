1b53ca0252ff3d444890871c41ff64a973aa6474
Harshal Dhongadi <harshal.dhongadi@anmsoft.com>
2024-05-09 15:25:50 +0530

    Added Endpoint for RequestPart type

--- src/main/java/com/multipartConcept/controller/MultipartController.java
+++ src/main/java/com/multipartConcept/controller/MultipartController.java
@@ -4,15 +4,16 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.multipartConcept.entity.MultiPart;
+import com.multipartConcept.entity.FileData;
 import com.multipartConcept.service.MultipartService;
 
 @RestController
@@ -22,15 +23,19 @@ public class MultipartController {
 	@Autowired
 	private MultipartService multipartService;
 
-	@PostMapping("/upload")
-	public ResponseEntity<MultiPart> uploadFiles(@RequestBody MultipartFile file)
+	@PostMapping(value = "/upload", consumes = { 
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.MULTIPART_FORM_DATA_VALUE,
+			MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<FileData> uploadFiles(@RequestPart("data") String data,
+			@RequestPart("file1") MultipartFile file1, @RequestPart("file2") MultipartFile file2)
 			throws IllegalStateException, IOException {
 
-		return ResponseEntity.ok(multipartService.saveFile(file));
+		return ResponseEntity.ok(multipartService.saveFile(data, file1, file2));
 	}
 
 	@GetMapping("/getFiles")
-	public ResponseEntity<List<MultiPart>> getAllFiles() throws IllegalStateException, IOException {
+	public ResponseEntity<List<FileData>> getAllFiles() throws IllegalStateException, IOException {
 
 		return ResponseEntity.ok(multipartService.getAllFiles());
 	}
--- src/main/java/com/multipartConcept/repository/MultipartRepository.java
+++ src/main/java/com/multipartConcept/repository/MultipartRepository.java
@@ -3,9 +3,9 @@ package com.multipartConcept.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.multipartConcept.entity.MultiPart;
+import com.multipartConcept.entity.FileData;
 
 @Repository
-public interface MultipartRepository extends JpaRepository<MultiPart, Integer> {
+public interface MultipartRepository extends JpaRepository<FileData, Integer> {
 
 }
